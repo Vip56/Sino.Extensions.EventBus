10967ab352132bd92f8b8d276dc9d0476177065e
姚正发 <976691141@qq.com>
2017-10-17 09:10:51 +0800

    添加忽略的文件

---
+++ src/Sino.Extensions.EventBus/Configuration/Publish/PublishConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace Sino.Extensions.EventBus.Configuration
+{
+    public class PublishConfiguration
+    {
+        public ExchangeConfiguration Exchange { get; set; }
+        public string RoutingKey { get; set; }
+        public Action<IBasicProperties> PropertyModifier { get; set; }
+        public EventHandler<BasicReturnEventArgs> BasicReturn { get; set; }
+    }
+}
